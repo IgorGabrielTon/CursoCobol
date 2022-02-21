da109ae182abcd9d9c6e975e5430c2f2d1bcd011
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-20 21:28:43 -0300

    Create OlaMundo.cob

---
+++ Aula01/OlaMundo.cob
@@ -0,0 +1,18 @@
+       *>Comentário é feito assim XD <*
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PrimeiroApp.
+       AUTHOR. IgorGabriel.
+       INSTALLATION. NomeEmpresa.
+       DATE-WRITTEN. 20-02-2022.
+       DATE-COMPILED. 20-02-2022.
+       SECURITY. SEM CRIPITOGRAFIA.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       OBJECT-COMPUTER. SAMSUNG.
+       SOURCE-COMPUTER. SAMSUNG.
+       DATA DIVISION.
+       
+       
+       PROCEDURE DIVISION.
+           DISPLAY "Olá Mundo!".
+           STOP RUN.
